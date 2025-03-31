62919365467c9f893f87752ee21caae4f2738647
Dhruvateja Amancha <123286539+Dhruva1129@users.noreply.github.com>
2025-03-31 21:54:54 +0530

    Update main.py

--- server/main.py
+++ server/main.py
@@ -34,7 +34,7 @@ app = FastAPI()
 # Enable CORS for frontend communication
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # Update if different frontend URL
+    allow_origins=["https://text-enhancer-pi.vercel.app/"],  # Update if different frontend URL
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
