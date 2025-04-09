33eba4a4b6e80686d13b78dd4edd72d0dbffc7f7
Dhruvateja Amancha <123286539+Dhruva1129@users.noreply.github.com>
2025-04-09 17:39:52 +0530

    Update App.jsx

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
 import TextEnhancer from "./components/Enhance/TextEnhancer";
 import ImageToTextEnhancer from "./components/ImageEnhance/ImageToTextEnhancer";
+import Summarizer from "./components/Summarizer/Summarizer";
 import "./App.css";
 import React from "react";
 import { useState } from "react";
@@ -16,6 +17,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/text-enhancer" element={<TextEnhancer />} />
         <Route path="/image-to-text-enhancer" element={<ImageToTextEnhancer />} />
+        <Route path="/summarizer" element={<Summarizer />} />
         {/* <Route path="/speak" element={<h1 className="content">Text-to-Speech</h1>} /> */}
       </Routes>
       
